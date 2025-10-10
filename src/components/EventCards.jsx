8ff974e7ba41361cfb811df9bcd1f2d80cc23d81
Zaid <zaidkhan45664@gmail.com>
2025-10-10 23:07:54 +0530

    I have update the Quicktory.

--- src/components/EventCards.jsx
+++ src/components/EventCards.jsx
@@ -36,27 +36,3 @@ export default EventCards;
 
 
 
-// function EventCard({ year,title,description, links }) {
-//   return (
-//     <div className="bg-white p-4 shadow rounded border-l-4 border-blue-500">
-//       <h3 className="text-lg font-semibold">🗓️ {year}</h3>
-//       <p className="text-gray-700 mt-2">{title}</p>
-//       <p className="text-gray-700 mt-2">{description}</p>
-
-
-//       {/* Wikipedia Link */}
-//       {links && links.length > 0 && (
-//         <a
-//           href={links[0].wikipedia}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className="text-blue-600 underline mt-2 inline-block"
-//         >
-//           🔗 Read more on Wikipedia
-//         </a>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default EventCard;
